487f0a7cffe2ad571f02f9ec1196876bed7a87ec
Ilya Polovintsev <tripforce@yandex.ru>
2024-07-27 14:38:49 +0300

    first commit

--- src/test/java/com/example/FelineTest.java
+++ src/test/java/com/example/FelineTest.java
@@ -1,2 +1,29 @@
-package com.example;public class FelineTest {
+package com.example;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class FelineTest {
+
+    Feline feline = new Feline();
+    List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
+
+    @Test
+    public void eatMeatTest() throws Exception {
+        assertEquals(expectedFood, feline.eatMeat());
+    }
+
+    @Test
+    public void getFamilyTest() {
+        assertEquals("Кошачьи", feline.getFamily());
+    }
+
+    @Test
+    public void getKittensNoCountTest() {
+        assertEquals(1, feline.getKittens());
+    }
+
 }
--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -1,2 +1,47 @@
-package com.example;public class LionTest {
+package com.example;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LionTest {
+    List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
+    Lion lion;
+    @Mock
+    Feline feline;
+
+    @Before
+    public void setUp() throws Exception {
+        lion = new Lion("Самец", feline);
+    }
+
+    @Test
+    public void getKittensTest(){
+        Mockito.when(feline.getKittens()).thenReturn(1);
+        assertEquals(1, lion.getKittens());
+    }
+
+    @Test
+    public void getFoodTest() throws Exception {
+        Mockito.when(feline.getFood("Хищник")).thenReturn(expectedFood);
+        assertEquals(expectedFood, lion.getFood());
+    }
+
+    @Test
+    public void lionExceptionTest() {
+        try {
+            Lion lion = new Lion("нечто", feline);
+        } catch (Exception exception) {
+            assertEquals("Используйте допустимые значения пола животного - самец или самка", exception.getMessage());
+        }
+    }
+
 }
